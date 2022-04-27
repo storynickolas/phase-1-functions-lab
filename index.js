ad079431ee057230f4d6adc95d801d88214f5915
Nick Story <storynickolasl@gmail.com>
2022-04-27 08:49:23 -0400

    commit tree d49752e159fbde1ea864451f2fb1015448e659f6

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(distance) {
+  let numBlock
+  distance > 42 ? numBlock = distance - 42 : numBlock = 42 - distance
+  return numBlock
+}
+
+function distanceFromHqInFeet(distance) {
+  return distanceFromHqInBlocks(distance) * 264
+}
+
+function distanceTravelledInFeet(block1, block2) {
+  return Math.abs((block2 - block1) * 264)
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(destination, start);
+  if(distance <= 400) {
+    return 0;
+  }
+  else if(distance < 2000) {
+    return (distance - 400) * 0.02;
+  }
+  else if(distance > 2000 && distance < 2500) {
+    return 25;
+  }
+  else {
+    return 'cannot travel that far';
+  }
+}
